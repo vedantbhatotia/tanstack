b1124269caa23c5a5240e929df302e7520e21dad
vedantbhatotia <vedantbhatotia@gmail.com>
2024-10-14 10:38:33 +0530

    query on click

--- src/components/postrq.jsx
+++ src/components/postrq.jsx
@@ -1,12 +1,17 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query"
 function Postrq() {
-    const {data,isError,isLoading,error} = useQuery({
+    const {data,isError,isLoading,error,refetch} = useQuery({
          queryKey: ['posts'],
          queryFn:async()=>{
             const response = await axios.get('http://localhost:4000/posts');
             return response
-        }
+        },
+        enabled:false,
+        // refetchInterval: 1000
+        refetchIntervalInBackground: true,
+        // used when we want to update the data in the background while we are not on the same page/component
+        // used for polling data in regular intervals eg in trading applications where we need to update the data in real time
     }) 
     if(isLoading){
         return <div>Loading...</div>
@@ -16,6 +21,8 @@ function Postrq() {
     }
     // console.log(data) 
     return (
+        <>
+         <button onClick={refetch}>Fetch Data</button>
         <div className="post-list">
         {data?.data.map(post => (
             <div onClick={() => handleClick(post.id)} key={post.id} className="post-item">
@@ -24,6 +31,7 @@ function Postrq() {
             </div>
         ))}
     </div>
+        </>
   )
 }
 export default Postrq
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { QueryClientProvider,QueryClient} from '@tanstack/react-query'
+import { QueryClientProvider,QueryClient} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={new QueryClient()}>
-    <App />
+          <App />
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
     </QueryClientProvider>
   </StrictMode>,
 )
